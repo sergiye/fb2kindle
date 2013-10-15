a9d2bce012bb2b95bba466e751d140c5207daf1d
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2013-10-15 18:39:16 +0300

    automatic conversion script

---
+++ convertAll.cmd
@@ -0,0 +1,28 @@
+@ECHO OFF
+set fb2folder="books"
+::set fb2folder="%~1"
+::set fb2folder=%CD%
+
+::Распаковать
+for /R "%fb2folder%\" %%a in ("*.fb2.zip") do "%~dp07za.exe" e "%%~a" -y -o"%%~dpa"
+::Удалить архивы
+for /R "%fb2folder%\" %%a in ("*.fb2.zip") do del /f /q "%%~a"
+
+::Для работы на киндле
+GOTO Kindle
+::SET /P mode=Enter 'a' if you start it from Kindle: 
+::IF "%mode%"=="a" GOTO Kindle
+::Сконвертировать
+for /R "%fb2folder%\" %%a in ("*.fb2") do "%~dp0Fb2Kindle.exe" "%%~a" -css styles.css -d -nh
+GOTO End
+:Kindle
+::Сконвертировать
+for /R "%fb2folder%\" %%a in ("*.fb2") do "%~dp0Fb2Kindle.exe" "%%~a" -d -nh
+move /Y "%fb2folder%\*.mobi" ..\documents
+:End
+
+::Упаковать
+::for /R "%fb2folder%\" %%a in ("*.fb2") do "%~dp07za.exe" a -tzip -y -o"%%~dpa" -mx9 "%%~a.zip" "%%~a"
+:: Удалить fb2 файлы
+::for /R "%fb2folder%\" %%a in ("*.fb2") do del /f /q "%%~a"
+::pause
