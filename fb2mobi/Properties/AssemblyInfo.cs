2f99857e4dac091b53063d446fe79940e7bc9c0d
simpl <egoshin.sergey@gmail.com>
2013-10-22 10:43:08 +0300

    add fb2mobi project

---
+++ fb2mobi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("fb2mobi")]
+[assembly: AssemblyDescription("fb2 to Mobipocket easy converter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fb2mobi")]
+[assembly: AssemblyCopyright("Sk0rp")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8949428-e6d1-49b4-9798-d97d48c8c4f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
---
+++ fb2mobi/Worker.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+using CommandLine.Utility;
+
+namespace fb2mobi
+{
+    class Worker
+    {
+        bool error_;
+
+        Arguments arguments_;
+        
+        string bookname_;
+        string outputdir_;
+        string inputFile_;
+        string workDir_;
+
+        public static void print_usage()
+        {
+            Console.WriteLine("  -us\t Use source file name as output.");
+            Console.WriteLine("  -nt\t No translite output file name.");
+        }
+
+        public bool error()
+        {
+            return error_;
+        }
+
+        public string getBookName(string ext)
+        {
+            return bookname_ + ext;
+        }
+
+        public string getBookName()
+        {
+            return getBookName("");
+        }
+
+        public string getWorkDir()
+        {
+            return workDir_;
+        }
+
+        public string getOutputDir()
+        {
+            return outputdir_;
+        }
+
+        public string getInputFile()
+        {
+            return inputFile_;
+        }
+
+        public Worker(Arguments args)
+        {
+            error_      = true;
+            
+            arguments_  = args;
+           
+            bookname_   = "";
+            outputdir_  = "";
+            inputFile_  = Path.GetFullPath(arguments_[0]);
+            workDir_    = "";
+
+            if (!File.Exists(inputFile_))
+                return;
+
+            string inputFileDir     = Path.GetDirectoryName(inputFile_);
+            string outputFileDir    = inputFileDir;
+            string bookname         = "";
+
+            // first. get a book file name from explicit output file
+            string of = arguments_[1];
+            if (of.Length != 0)
+            {
+                try
+                {
+                    if(Directory.Exists(of)){ // it'a a dir
+                        outputFileDir = of;
+                    }
+                    else {
+                        bookname = Path.GetFileNameWithoutExtension(of);
+
+                        string wd = Path.GetDirectoryName(of);
+                        if (wd.Length != 0)
+                            if (Directory.Exists(wd))
+                                outputFileDir = wd;
+                    }
+
+                }
+                catch (Exception)
+                {
+                    outputFileDir   = inputFileDir;
+                    bookname        = "";
+                }
+            }
+
+            // get a book file name from input file name or from book title
+            if (bookname.Length == 0)
+            {
+                bookname = Path.GetFileNameWithoutExtension(inputFile_);
+
+                if (arguments_["us"] != "true")
+                {
+                    try {
+                        // get book name
+                        XmlDocument dd = new XmlDocument();
+                        dd.Load(inputFile_);
+                        XmlNode root = dd["FictionBook"]["description"], data;
+                        if ((data = root["title-info"]) != null)
+                        {
+                            data = data["book-title"];
+                            if (data != null)
+                            {
+                                string val = data.InnerText;
+                                if (val.Length > 0)
+                                    bookname = val;
+                            }
+                        }
+                    } catch (Exception) { }
+                }
+
+                if (arguments_["nt"] != "true")
+                    bookname = transliteName(bookname);
+            }
+
+            if (bookname.Length == 0)
+                bookname = "fb2mobi";
+
+            char[] trimchars = { '\\' };
+            outputFileDir = outputFileDir.TrimEnd(trimchars);
+            inputFileDir = inputFileDir.TrimEnd(trimchars);
+
+            bool ok = prepareOutputFilePlace(outputFileDir, bookname);
+            if(!ok && outputFileDir != inputFileDir)
+                ok = prepareOutputFilePlace(inputFileDir, bookname);
+            if(!ok)
+                ok = prepareOutputFilePlace(Path.GetTempPath()+ "\\fb2mobi", bookname);
+
+            if (ok)
+            {
+                if (arguments_["cl"] == "true")
+                {
+                    string testdir = Path.GetTempPath() + "\\fb2mobi_";
+                    for (int cont = 0; ; ++cont)
+                    {
+                        workDir_ = testdir;
+                        workDir_ += cont.ToString();
+                        if (!File.Exists(workDir_) && !Directory.Exists(workDir_))
+                            break;
+                    }
+                }
+                else
+                {
+                    workDir_ = outputdir_;
+                    workDir_ += bookname_;
+                }
+
+                Directory.CreateDirectory(workDir_);
+                workDir_ += "\\";
+            }
+
+            error_ = !ok;
+        }
+
+
+        bool prepareOutputFilePlace(string dir, string file)
+        {
+            try
+            {
+                string name;
+                for(int cont = 0;; ++cont){
+                    name = file;
+                    if(cont != 0)
+                        name += cont.ToString();
+                    string testName = dir + "\\" + name;
+                    if (!File.Exists(testName + ".mobi") && !Directory.Exists(testName))
+                        break;
+                }
+
+                bookname_ = name;
+                outputdir_ = dir + "\\";
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        string transliteName(string file)
+        {
+            char[] rus = new char[] {
+                    'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й'
+                  , 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у'
+                  , 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'э', 'ю', 'я'
+                  , 'ь', '\\',':', '/', '?', '*', ' '};
+            char[] lat = new char[] {
+                    'a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'y'
+                  , 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u'
+                  , 'f', 'h', 'c', 'h', 's', 's', 'i', 'e', 'u', 'a'
+                  , '\'', '_', '_', '_', '_', '_', '_'};
+            string name = "";
+            for (int idx = 0; idx < file.Length; ++idx)
+            {
+                char ch = char.ToLower(file[idx]);
+                if (ch == '.')
+                    break;
+                int i = Array.FindIndex<char>(rus, delegate(char c) { return c == ch; });
+                if (i >= 0)
+                    name += lat[i];
+                else if (ch >= '0' && ch <= 127)
+                    name += ch;
+                if (name.Length > 31)
+                    break;
+            }
+            return name;
+        }
+
+        public void saveImages()
+        {
+            XmlDocument dd = new XmlDocument();
+            dd.Load(inputFile_);
+            XmlNode bin = dd["FictionBook"]["binary"];
+            while (bin != null)
+            {
+                FileStream fs = new FileStream(getWorkDir() + bin.Attributes["id"].InnerText, FileMode.Create);
+                BinaryWriter w = new BinaryWriter(fs);
+                w.Write(Convert.FromBase64String(bin.InnerText));
+                w.Close();
+                fs.Close();
+                bin = bin.NextSibling;
+            }
+        }
+
+        public void transform(string xsl, string name)
+        {
+            XmlTextReader reader = new XmlTextReader(inputFile_);
+
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(xsl);
+
+            XmlTextWriter writer = new XmlTextWriter(getWorkDir() + name, null);
+            writer.Formatting = Formatting.Indented;
+
+            xslt.Transform(reader, null, writer, null);
+
+            writer.Close();
+        }
+    }
+}
