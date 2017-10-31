3a66218daa71bbafa6501149eb7992d7404611c1
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2017-10-31 19:12:43 +0200

    convert to mobi added

--- jail/Controllers/HomeController.cs
+++ jail/Controllers/HomeController.cs
@@ -2,19 +2,17 @@
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Web.Configuration;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Fb2Kindle;
 using Ionic.Zip;
 using jail.Classes;
 
 namespace jail.Controllers
 {
+    [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
     public class HomeController : Controller
     {
         protected override void OnException(ExceptionContext filterContext)
@@ -84,15 +82,49 @@ namespace jail.Controllers
                 {
                     zipEntry.Extract(ms);
                     ms.Seek(0, SeekOrigin.Begin);
-                    var fileName = Regex.Replace(string.Format("{0}_{1}.fb2", 
-                        book.Authors.First().FullName.ToLower().Translit(), 
-                        book.Title.ToLower().Translit()), 
-                        @"[!@#$%_ ']", "_");
-                    return File(ms.ToArray(), System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+                    return File(ms.ToArray(), System.Net.Mime.MediaTypeNames.Application.Octet, 
+                        CommonHelper.GetBookDownloadFileName(book));
                 }
             }
         }
 
+        [HttpGet, Route("mobi")]
+        public FileResult Mobi(long id)
+        {
+            var book = DataRepository.GetBook(id);
+            if (book == null)
+                throw new FileNotFoundException("Book not found in db");
+
+            var resultFile = Server.MapPath(string.Format("~/Uploads/{0}", Path.ChangeExtension(book.FileName, ".mobi")));
+            if (!System.IO.File.Exists(resultFile))
+            {
+                var tempFile = Path.Combine(Path.GetTempPath(), book.FileName);
+                var archPath = Path.Combine(DataRepository.ArchivesPath, book.ArchiveFileName);
+                if (!System.IO.File.Exists(archPath))
+                {
+                    throw new FileNotFoundException("Book archive not found");
+                }
+                using (var zip = new ZipFile(archPath))
+                {
+                    var zipEntry = zip.Entries.FirstOrDefault(e => e.FileName.Equals(book.FileName));
+                    if (zipEntry == null)
+                        throw new FileNotFoundException("Book file not found in archive");
+                    using (var fs = System.IO.File.Create(tempFile))
+                        zipEntry.Extract(fs);
+                }
+
+                var conv = new Convertor(SettingsHelper.ConverterSettings, SettingsHelper.ConverterCss, SettingsHelper.ConverterDetailedOutput);
+                if (!conv.ConvertBook(tempFile, false))
+                    throw new ArgumentException("Error converting book for kindle");
+
+                tempFile = Path.ChangeExtension(tempFile, ".mobi");
+                System.IO.File.Move(tempFile, resultFile);
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(resultFile);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, 
+                CommonHelper.GetBookDownloadFileName(book, ".mobi"));
+        }
 
         [HttpGet, Route("read")]
         public ActionResult Read(long id)
@@ -172,7 +204,7 @@ namespace jail.Controllers
             return View();
         }
 
-        public ActionResult File()
+        public ActionResult UploadFile()
         {
             ViewBag.maxRequestLength = maxRequestLength;
             return View();
