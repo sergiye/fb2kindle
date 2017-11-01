35662a3990b2081559dcc13660327d5de48a0f06
Sergey Egoshin <egoshin.sergey@gmail.com>
2017-11-01 15:29:26 +0200

    db requests optimized (bookdetailedinfo class extracted)

--- jail/Models/AuthorInfo.cs
+++ jail/Models/AuthorInfo.cs
@@ -4,13 +4,10 @@ namespace jail.Models
 {
     public class AuthorInfo: LongIdContainer
     {
-        public string Letter { get; set; }
         public string FullName { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-        public string SearchName { get; set; }
-        public long Number { get; set; }
 
         public string DisplayName
         {
