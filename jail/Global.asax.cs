a9cfa9cec30f5cda6b97935495bbf5877d975bc2
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2018-12-06 21:10:30 +0200

    error view fix

--- jail/Global.asax.cs
+++ jail/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,37 @@ namespace jail
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            HttpContext httpContext = HttpContext.Current;
+            if (httpContext != null)
+            {
+                RequestContext requestContext = ((MvcHandler)httpContext.CurrentHandler).RequestContext;
+                /* When the request is ajax the system can automatically handle a mistake with a JSON response. 
+                   Then overwrites the default response */
+                if (requestContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    httpContext.Response.Clear();
+                    string controllerName = requestContext.RouteData.GetRequiredString("controller");
+                    IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+                    IController controller = factory.CreateController(requestContext, controllerName);
+                    ControllerContext controllerContext = new ControllerContext(requestContext, (ControllerBase)controller);
+
+                    JsonResult jsonResult = new JsonResult
+                    {
+                        Data = new { success = false, serverError = "500" },
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                    jsonResult.ExecuteResult(controllerContext);
+                    httpContext.Response.End();
+                }
+                else
+                {
+                    httpContext.Response.Redirect("~/Error");
+                }
+            }
         }
     }
 }
