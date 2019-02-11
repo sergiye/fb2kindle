96997d35c070a16e7b646e129f3fdecec13755c2
Sergey Egoshin <egoshin.sergey@gmail.com>
2019-02-11 18:53:22 +0200

    prettyxml added

---
+++ PrettyXml/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrettyXml")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergiy Yegoshyn")]
+[assembly: AssemblyProduct("PrettyXml")]
+[assembly: AssemblyCopyright("Copyright © Sergiy Yegoshyn 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ed33f24-618b-42cb-b75b-727bec7419d9")]
+
+[assembly: AssemblyVersion("1.0.*")]
