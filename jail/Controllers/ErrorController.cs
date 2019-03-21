cd0fed2cde9833ec601d21a6758a252280c55001
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2019-03-21 18:29:01 +0200

    error processing added to jail

---
+++ jail/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+namespace jail.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View("NotFound");
+        }
+    }
+}
