e5eff065d3674572dcc29d41822fd572a50679f4
Sergey Egoshin <egoshin.sergey@gmail.com>
2019-04-25 20:59:32 +0300

    books history added

---
+++ jail/Models/BookHistoryInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace jail.Models
+{
+    public class BookHistoryInfo: BookInfo
+    {
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime GeneratedTime { get; set; }
+    }
+}
