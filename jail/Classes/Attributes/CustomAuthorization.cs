361b780adc3260cb938f785db9b39a0fb5b129e6
Sergey Egoshin <egoshin.sergey@gmail.com>
2019-04-26 09:44:20 +0300

    session restore attribute added; authorization improved

--- jail/Classes/Attributes/CustomAuthorization.cs
+++ jail/Classes/Attributes/CustomAuthorization.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
 using jail.Models;
 
 namespace jail.Classes.Attributes
@@ -12,26 +11,11 @@ namespace jail.Classes.Attributes
 
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            if (httpContext.Session["User"] == null)
-            {
-                var authCookie = httpContext.Request.Cookies.Get(FormsAuthentication.FormsCookieName);
-                if (authCookie == null) return false;
-
-                var ticket = FormsAuthentication.Decrypt(authCookie.Value);
-                if (ticket == null) return false;
-                var userData = ticket.UserData.Split(',');
-                if (userData.Length != 2) return false;
-
-                var user = UserRepository.GetUserById(int.Parse(userData[0]));
-                if (user == null) return false;
-                httpContext.Session["User"] = user;
-                Logger.WriteInfo(string.Format("{0} is back", user.UserType), CommonHelper.GetClientAddress(), user.Email);
-            }
+            var user = httpContext.Session["User"] as UserProfile;
+            if (user == null) return false;
 
             if (Roles == null || Roles.Length <= 0) return false;
-            
-            var sessionUser = (UserProfile) httpContext.Session["User"];
-            var userType = sessionUser.UserType;
+            var userType = user.UserType;
             return userType == UserType.Administrator || Roles.Any(role => role == userType);
         }
     }
---
+++ jail/Classes/Attributes/UserTypeFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using jail.Models;
+
+namespace jail.Classes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class UserTypeFilterAttribute : ActionFilterAttribute
+    {
+        public UserType[] Roles { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.RequestContext.HttpContext.Session["User"] is UserProfile user)
+            {
+                if (Roles != null && Roles.Length > 0)
+                {
+                    var userType = user.UserType;
+                    if (userType == UserType.Administrator || Roles.Any(role => role == userType))
+                    {
+                        return; //all is ok
+                    }
+                }
+            }
+            filterContext.Result = new HttpCodeActionResult(HttpStatusCode.MethodNotAllowed, "This user type is not allowed");
+        }
+    }
+}
