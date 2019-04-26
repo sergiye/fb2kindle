e031d45f0ead2fc0bc5b844e3628cad60b51b5ac
Sergey Egoshin <egoshin.sergey@gmail.com>
2019-04-26 10:20:18 +0300

    SkipLoggingAttribute added

--- jail/Classes/Attributes/ActionLoggerAttribute.cs
+++ jail/Classes/Attributes/ActionLoggerAttribute.cs
@@ -4,17 +4,32 @@ using System.Web.Mvc;
 namespace jail.Classes.Attributes
 {
     /// <summary>
+    /// Attribute to mark methods to be skipped for logging
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SkipLoggingAttribute : Attribute
+    {
+    }
+
+    /// <summary>
     /// Action Logger attribute
     /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
     public class ActionLoggerAttribute : ActionFilterAttribute
     {
         /// <inheritdoc />
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             var request = filterContext.HttpContext.Request;
-            if (request.HttpMethod == "GET"
-                && (request.Path.Equals("/log", StringComparison.OrdinalIgnoreCase) || request.Path.Equals("/logp", StringComparison.OrdinalIgnoreCase)))
-                return;
+            foreach (Attribute a in filterContext.ActionDescriptor.GetCustomAttributes(false))
+            {
+                if (a.GetType() == typeof(SkipLoggingAttribute))
+                    return;
+            }
+//            if (request.HttpMethod == "GET"
+//                && (request.Path.Equals("/log", StringComparison.OrdinalIgnoreCase) || request.Path.Equals("/logp", StringComparison.OrdinalIgnoreCase)))
+//                return;
+
             Logger.WriteTrace(CommonHelper.GetActionLogName(request), 
                 CommonHelper.GetClientAddress(), CommonHelper.CurrentIdentityName);
         }
