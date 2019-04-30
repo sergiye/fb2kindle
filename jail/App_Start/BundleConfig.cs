09cac3a4737a00e1969a5aaa8ab901d2e4246d97
Sergey Egoshin <egoshin.sergey@gmail.com>
2019-04-30 10:10:57 +0300

    respond removed; packages updated

--- jail/App_Start/BundleConfig.cs
+++ jail/App_Start/BundleConfig.cs
@@ -21,8 +21,9 @@ namespace jail
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"
+//                    , "~/Scripts/respond.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
