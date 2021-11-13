a15e2e3689fa7d73fac15eca598d64034ec38fd0
Sergey Egoshin <egoshin.sergey@gmail.com>
2021-11-13 10:31:37 +0200

    jail: GenerateBookDetails in BackgroundTasks queue (one by one)

---
+++ jail/Classes/BackgroundTasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Simpl.Extensions;
+
+namespace jail.Classes {
+  
+  internal static class BackgroundTasks {
+    
+    private static readonly CommonQueue<Action> actionsQueue;
+
+    static BackgroundTasks() {
+      actionsQueue = new CommonQueue<Action>("BackgroundActionsQueue");
+      actionsQueue.OnExecuteTask += action => {
+        try {
+          action.Invoke();
+        }
+        catch (Exception e) {
+          Console.WriteLine(e);
+        }
+      }; 
+      actionsQueue.Start();
+    }
+    
+    public static void EnqueueAction(Action executeMethod) {
+      actionsQueue?.EnqueueTask(executeMethod);
+    }
+  }
+}
