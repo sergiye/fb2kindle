6c033e2a9b80f990a53c4431231c846e935a1d0d
Sergey Egoshin <egoshin.sergey@gmail.com>
2021-11-14 11:37:35 +0200

    jail: set book lang butoon

--- Jail_express.cmd
+++ Jail_express.cmd
@@ -1,3 +1,3 @@
 start http://localhost:80/
 
-call ..\IISExpress\iisexpress.exe /config:./.idea/config/applicationhost.config
+call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\.idea\config\applicationhost.config
