0576746c9d5417baeea1b0c920caf77d01b0c6f7
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2021-12-24 09:30:42 +0200

    jail: Jail_express.config added

--- Jail_express.cmd
+++ Jail_express.cmd
@@ -1,3 +1,5 @@
 start http://localhost:80/
 
-call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\.idea\config\applicationhost.config
+call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\Jail_express.config
+
+rem pause
