262be85f0a534f2943e6df8ea01aa0617aecb2ae
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2022-08-16 17:45:46 +0300

    jail: ngrok updated

--- Jail_express.cmd
+++ Jail_express.cmd
@@ -1,6 +1,6 @@
 start http://localhost:80/fav?id=2
 
-start d:\portable\ngrok\ngrok.exe start -config ngrok.yml jail
+start d:\portable\ngrok\ngrok.exe start --config ngrok.yml jail
 
 call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\Jail_express.config
 
