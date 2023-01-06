68c8a423cfcbc4ecf7dad9fe595206ac7829a06b
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-01-06 14:17:03 +0200

    LibraryCleaner: add high-dpi support

--- Jail_express.cmd
+++ Jail_express.cmd
@@ -1,7 +1,7 @@
-start http://localhost:80/fav?id=2
+@start http://localhost:80/fav?id=2
 
-@rem start d:\portable\ngrok\ngrok.exe start --config ngrok.yml jail
+rem @start d:\portable\ngrok\ngrok.exe start --config ngrok.yml jail
 
-call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\Jail_express.config
+@call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\Jail_express.config
 
 rem pause
