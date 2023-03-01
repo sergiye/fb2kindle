adb61e43d6bda66ce807248a181aefab641c2f3a
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-03-01 16:16:50 +0200

    repo cleanup

--- research/fb2mobi/fb2mobiAll.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-set fb2folder="."
-
-for /R "%fb2folder%\" %%a in ("*.fb2.zip") do "%~dp07za.exe" e "%%~a" -y -o"%%~dpa"
-for /R "%fb2folder%\" %%a in ("*.fb2.zip") do del /f /q "%%~a"
-
-for /R "%fb2folder%\" %%a in ("*.fb2") do "%~dp0fb2mobi.exe" "%%~a" -us -nt -nc -cl
-
-pause
