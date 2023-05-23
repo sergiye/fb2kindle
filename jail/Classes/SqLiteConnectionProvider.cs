5e89e505449a68984926a3d4d470c00112555ff7
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-05-23 19:43:18 +0300

    Jail: dark theme

--- jail/Classes/SqLiteConnectionProvider.cs
+++ jail/Classes/SqLiteConnectionProvider.cs
@@ -2,52 +2,41 @@ using System;
 using System.Data;
 using System.Data.SQLite;
 using System.Threading.Tasks;
-using DapperExtensions.Sql;
 using Simpl.Extensions.Database;
 
-namespace jail.Classes
-{
-    internal class SqLiteConnectionProvider<IdType> : BaseConnectionProvider<IdType> where IdType : IEquatable<IdType>
-    {
+namespace jail.Classes {
+    internal class SqLiteConnectionProvider<IdType> : BaseConnectionProvider<IdType> where IdType : IEquatable<IdType> {
         private readonly bool failIfMissing;
 
-        protected string SqLiteConnectionString
-        {
-            get { return $"Data Source='{ConnectionString}';Version=3;FailIfMissing={failIfMissing};DateTimeKind=Utc;UTF8Encoding=True;synchronous = OFF;Journal Mode=Off;Page Size=4096;Cache Size=2000;"; }
-        }
+        protected string SqLiteConnectionString =>
+            $"Data Source='{ConnectionString}';Version=3;FailIfMissing={failIfMissing};DateTimeKind=Utc;UTF8Encoding=True;synchronous = OFF;Journal Mode=Off;Page Size=4096;Cache Size=2000;";
 
         internal SqLiteConnectionProvider(string databasePath, bool failIfMissing = true)
-            : base(databasePath)
-        {
-            DapperExtensions.DapperExtensions.SqlDialect = new SqliteDialect();
+            : base(databasePath) {
             this.failIfMissing = failIfMissing;
         }
 
         #region Basic Methods
 
-        public override IDbConnection GetConnection()
-        {
-            var cn = new SQLiteConnection(SqLiteConnectionString);            
-            cn.Open();                        
+        public override IDbConnection GetConnection() {
+            var cn = new SQLiteConnection(SqLiteConnectionString);
+            cn.Open();
             return cn;
         }
 
-        public override async Task<IDbConnection> GetConnectionAsync()
-        {
-            var cn = new SQLiteConnection(SqLiteConnectionString);            
-            await cn.OpenAsync();                        
+        public override async Task<IDbConnection> GetConnectionAsync() {
+            var cn = new SQLiteConnection(SqLiteConnectionString);
+            await cn.OpenAsync();
             return cn;
         }
 
-        public override IDbTransaction GetTransaction(IDbConnection connection)
-        {
+        public override IDbTransaction GetTransaction(IDbConnection connection) {
             if (connection.State != ConnectionState.Open)
                 connection.Open();
             return connection.BeginTransaction();
         }
 
-        public override IDbCommand GetCommand(string sqlString, IDbConnection cn)
-        {
+        public override IDbCommand GetCommand(string sqlString, IDbConnection cn) {
             var connection = cn as SQLiteConnection;
             return connection == null ? null : new SQLiteCommand(sqlString, connection);
         }
---
+++ jail/Scripts/main.js
@@ -0,0 +1,45 @@
+﻿function ShowImage(imghref) {
+    window.Helper.ShowNotificationPopup('<img width="570px" height="auto" src="' + imghref + '" />', window.HelperItem.NotificationType.Image);
+}
+
+function EnableDarkTheme(){
+    DarkReader.enable({ brightness: 100, contrast: 90, sepia: 10 });
+    localStorage.setItem("theme", "dark");
+    document.querySelector("#themeText").innerHTML = "Dark";
+}
+function DisableDarkTheme(){
+    DarkReader.disable();
+    localStorage.setItem("theme", "light");
+    document.querySelector("#themeText").innerHTML = "Light";
+}
+function ToggleTheme(){
+    // Check if Dark Reader is enabled.
+    const isEnabled = DarkReader.isEnabled();
+    // Stop watching for the system color scheme.
+    // DarkReader.auto(false);
+    if (isEnabled)
+        DisableDarkTheme();
+    else
+        EnableDarkTheme();
+}
+
+jQuery(function ($) {
+    $(document).ajaxStop(function () {
+        $("#ajax_loader").hide();
+    });
+    $(document).ajaxStart(function () {
+        $("#ajax_loader").show();
+    });
+
+    const currentTheme = localStorage.getItem("theme");
+    // if (currentTheme === undefined){
+    //     // Enable when the system color scheme is dark.
+    //     DarkReader.auto({ brightness: 100, contrast: 90, sepia: 10 });
+    // }
+    // else 
+    if (currentTheme === "dark") {
+        EnableDarkTheme();
+    }
+    // Get the generated CSS of Dark Reader returned as a string.
+    // const CSS = await DarkReader.exportGeneratedCSS();
+});
