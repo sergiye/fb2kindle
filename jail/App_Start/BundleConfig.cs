2d264824fd73bf6471929e8cfda96b3f7875fe53
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-05-23 19:43:18 +0300

    Jail: dark theme

--- jail/App_Start/BundleConfig.cs
+++ jail/App_Start/BundleConfig.cs
@@ -8,12 +8,14 @@ namespace jail
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-3.6.0.min.js",
+                        "~/Scripts/darkreader.min.js",
                         "~/Scripts/helper.js",
+                        "~/Scripts/main.js",
                         "~/Scripts/modal.popup.control.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            // bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //             "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -21,12 +23,13 @@ namespace jail
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"
+                      "~/Scripts/bootstrap.min.js"
 //                    , "~/Scripts/respond.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.min.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
