7f6c3dc28794997e7670799b2177db7bc7943a21
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-09-29 03:15:05 +0300

    cleanup

--- Jail_express.cmd
+++
@@ -1,9 +0,0 @@
-rem @start d:\portable\ngrok\ngrok.exe start --config ngrok.yml site admin
-rem @start https://physically-polite-loon.ngrok-free.app/fav?id=2
-
-@start http://localhost:80/fav?id=2
-rem @start http://localhost:4040
-
-@call d:\my\work\IISExpress\iisexpress.exe /config:d:\my\work\fb2kindle\Jail_express.config
-
-rem pause
--- research/test.cmd
+++
@@ -1,4 +0,0 @@
-cd test
-"..\tar" -cv --file="..\test.tar" *
-"..\gzip" -9 < "..\test.tar" > "..\test.mobi"
-del "..\test.tar"
