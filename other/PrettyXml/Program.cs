6500b706ecdaa6524456d86825a3b389a6ee870e
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2024-03-02 15:05:24 +0200

    jail show favorite date in hint

--- other/PrettyXml/Program.cs
+++ other/PrettyXml/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 
-namespace PrettyXml
-{
-    class Program
-    {
-        private static void PrettyXml(string xmlPath)
-        {
+namespace PrettyXml {
+    class Program {
+        private static void PrettyXml(string xmlPath) {
             var stringBuilder = new StringBuilder();
             var element = XElement.Parse(File.ReadAllText(xmlPath));
-            var settings = new XmlWriterSettings {OmitXmlDeclaration = true, Indent = true, NewLineOnAttributes = true};
-            using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
-            {
+
+            //XDocument doc = XDocument.Load(FILENAME);
+            element = Sort_XML.sort(element);
+
+            var settings = new XmlWriterSettings { OmitXmlDeclaration = true, Indent = true, NewLineOnAttributes = false };
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, settings)) {
                 element.Save(xmlWriter);
             }
             File.WriteAllText(xmlPath, stringBuilder.ToString());
         }
 
-        private static void PrettyJson(string jsonPath)
-        {
+        private static void PrettyJson(string jsonPath) {
             // var jss = new JavaScriptSerializer();
             // var data = jss.Deserialize<object>(File.ReadAllText(jsonPath));
             // var text = jss.Serialize(data);
@@ -43,24 +44,18 @@ namespace PrettyXml
             // File.WriteAllText(jsonPath, newText);
         }
 
-        static void Main(string[] args)
-        {
-            foreach (var filePath in args)
-            {
-                try
-                {
-                    if (File.Exists(filePath))
-                    {
+        static void Main(string[] args) {
+            foreach (var filePath in args) {
+                try {
+                    if (File.Exists(filePath)) {
                         Console.WriteLine("Processing: {0}", filePath);
                         PrettyXml(filePath);
                     }
-                    else
-                    {
+                    else {
                         Console.WriteLine("File not found: {0}", filePath);
                     }
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Console.WriteLine(ex);
                 }
             }
@@ -69,6 +64,34 @@ namespace PrettyXml
             else
                 Console.WriteLine("Done!");
             Console.ReadLine();
+        }
+    }
+
+    public class Sort_XML {
+        static public XElement sort(XElement root) {
+            XElement sortedRoot = new XElement(root.Name.LocalName);
+            sortRecursive(root, sortedRoot);
+            return sortedRoot;
+        }
+
+        public static void sortRecursive(XElement parent, XElement sortedRoot) {
+            foreach (XElement child in parent.Elements().OrderBy(x => AttributeValue(x, "Name", x.Name.LocalName))) {
+                XElement childElement = new XElement(child.Name.LocalName);
+                sortedRoot.Add(childElement);
+                childElement.Add(child.Attributes());
+                //foreach (XAttribute attribute in child.Attributes().OrderBy(x => x.Name.LocalName)) {
+                //    childElement.Add(new XAttribute(attribute.Name.LocalName, attribute.Value));
+                //}
+                if (child.HasElements) sortRecursive(child, childElement);
+            }
+        }
+
+        internal static string AttributeValue(XElement source, XName name, string defaultResult = null) {
+            var value = source.Attribute(name);
+            if (value == null || string.IsNullOrEmpty(value.Value.Trim()))
+                return defaultResult;
+            return value.Value.Trim();
+
         }
     }
 }
